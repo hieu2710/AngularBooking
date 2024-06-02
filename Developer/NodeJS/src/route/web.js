377ca6643cbbbe607ef6342999643fda3d36354a
hieu <trunghieu27102002@gmail.com>
2024-06-02 21:39:26 +0700

    First Commit

---
+++ Developer/NodeJS/src/route/web.js
@@ -0,0 +1,19 @@
+import express from "express"
+import homeController from "../controllers/homeController";
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+
+    router.get('/', homeController.getHomePage);
+    router.get('/about', homeController.getAboutPage);
+    router.get('/api', (req, res) => {
+        return res.send("Hello Híu!!!")
+    });
+    router.get('/crud', homeController.getCRUD);
+
+    router.post('/post-crud', homeController.postCRUD);
+
+    return app.use("/", router);
+}
+
+module.exports = initWebRoutes;
